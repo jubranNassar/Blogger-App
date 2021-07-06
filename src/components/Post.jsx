ddf47d7f2bd828a9082ca9d5c8816c6c04cf22ed
jubrannassar <juby.nassar123@gmail.com>
2021-07-06 13:31:03 -0400

    adds new time-posted  feature

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -5,35 +5,24 @@ import Like from "./Like";
 function Post(props) {
   const { post, setSaved } = props;
 
-  // const time = new Date(post.fields.Created).toISOString().substring(11, 16)
-  // const newTime = time.splice(2,2)
-  // console.log(newTime)
-  
-      // const getFormattedTime = (time)=> {
-      //   const hours24 = parseInt(time.substring(0,2));
-      //   const hours = ((hours24 + 11) % 12) + 1;
-      //   const amPm = hours24 > 11 ? 'pm' : 'am';
-      //   const minutes = time.substring(3);
-        
-      //   return hours + ':' + minutes + amPm;
-      // };
-
-      // getFormattedTime(time);
-    
-  const handleClick = (e) => {
-    setSaved((saved) => {
-      // if this posts id is in the saved array then return the saved array without this posts id
-      if (saved.includes(post.id)) {
-        return saved.filter((savedPost) => savedPost.id !== post.id);
-      } else {
-        // otherwise return all previous saved posts and this post
-        return [...saved, post];
-      }
-    });
-  };
-  return (
-    <div className="post">
-      <h1 className="name">{post.fields.name}</h1>
+  const time = new Date(post.fields.Created).toLocaleString()    
+      const handleClick = (e) => {
+        setSaved((saved) => {
+          // if this posts id is in the saved array then return the saved array without this posts id
+          if (saved.includes(post.id)) {
+            return saved.filter((savedPost) => savedPost.id !== post.id);
+          } else {
+            // otherwise return all previous saved posts and this post
+            return [...saved, post];
+          }
+        });
+      };
+      return (
+        <div className="post">
+          <div id="name-and-time">
+        <h4 id="time">{time.toLocaleString().split(",")[1]}</h4>
+        <h1 className="name">{post.fields.name}</h1>
+          </div>
       <p className="post-text">{post.fields.post}</p>
       <CommentForm post={props.post} setToggleFetch={props.setToggleFetch} />
       <ul id="comments">
